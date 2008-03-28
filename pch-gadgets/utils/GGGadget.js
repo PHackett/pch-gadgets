a6a746c5e2f951936924bcc7512aef72b6e0b74f
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2008-03-28 14:13:49 +0000

    Ability to detect if gadget is embedded in Google sites

--- pch-gadgets/utils/GGGadget.js
+++ pch-gadgets/utils/GGGadget.js
@@ -2,6 +2,7 @@
 // Some statics for my Google gadgets
 //
 var gGGGadget_Root="http://pch-gadgets.googlecode.com/svn/trunk/pch-gadgets/";
+var gGGGadget_SitesRoot="http://sites.google.com/a/trcc.paulhackett.com/trcc-cricket-club/";
 
 
 
@@ -39,10 +40,12 @@ function GGGadget_getHostURL ()
 //-----------------------------------[GGUtils_parseHostQuery]-
 // Parse the query parameters from the hosting page 
 //
+//	@param	aDelim	IN	Delimiter to use in the query
+//
 // @return Associative array of items 
 //
 //------------------------------------------------------------
-function GGGadget_parseHostQuery ()
+function GGGadget_parseHostQuery (aDelim)
 {
 	var lArgs = new Object();
 	var lURL = GGGadget_getHostURL();
@@ -58,8 +61,28 @@ function GGGadget_parseHostQuery ()
 	}
 	else
 	{
-		lArgs = GGUtils_ParseQuery (lURL.substring (lIndex+1));
+		lArgs = GGUtils_ParseQuery (lURL.substring (lIndex+1), aDelim);
 	}
 	
 	return (lArgs);
+}
+
+
+//-------------------------------------[GGUtils_hostedOnSites]-
+// Is the gadget being hosted on google sites? 
+//
+//	@return true if so, else false 
+//
+//------------------------------------------------------------
+function GGGadget_hostedOnSites ()
+{
+	var lRet=false;
+	var lIndex=GGGadget_getHostURL().indexOf(gGGGadget_SitesRoot);
+
+	if (0 == lIndex)
+	{
+		lRet = true;
+	}	
+	
+	return (lRet);
 }
