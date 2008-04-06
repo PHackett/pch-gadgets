3eab50b54a21b5c80233ad171418709af59a4d09
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2008-04-06 14:16:07 +0000

    Initial parse

--- pch-gadgets/TRCC/objects/CricketMatch.js
+++ pch-gadgets/TRCC/objects/CricketMatch.js
@@ -9,4 +9,36 @@
 //------------------------------------------------------------
 function GGTRCC_CricketMatchO (aCricketMatchXML)
 {
+	//
+	// Extract the data from the attributes
+	//
+	this.mOppo	= aCricketMatchXML.getAttribute ("oppo");
+	this.mDate	= new Date (aCricketMatchXML.getAttribute ("date"));
+
+	//
+	// Methods
+	//
+	this.oppo 			= GGTRCC_CricketMatchO___oppo;
+	this.date 			= GGTRCC_CricketMatchO___date;
+	this.matchHTML 		= GGTRCC_CricketMatchO___matchHTML;
+}
+
+function GGTRCC_CricketMatchO___oppo()	{ return (this.mOppo);	}
+function GGTRCC_CricketMatchO___date()	{ return (this.mDate);	}
+
+
+//-------------------------[GGTRCC_CricketMatchO___matchHTML]-
+// Return the match as HTML
+//
+// @return	HTML
+//
+//------------------------------------------------------------
+function GGTRCC_CricketMatchO___matchHTML()
+{
+	var lRet="";
+	
+	lRet += "Opposition = " + this.oppo() + "<br>";
+	lRet += "Date = " + this.date().toString() + "<br>";
+	
+	return (lRet);
 }
