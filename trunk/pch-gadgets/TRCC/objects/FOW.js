01cad1cfadfa58d0999bc463c80fbd0f0490b164
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2008-04-07 19:48:38 +0000

    Now the fall-of-wickets rendering.

--- trunk/pch-gadgets/TRCC/objects/FOW.js
+++ trunk/pch-gadgets/TRCC/objects/FOW.js
@@ -29,3 +29,90 @@ function GGTRCC_FOWO___HTML()
 
 function GGTRCC_FOWO___batsmanNo()	{ return (this.mBatsmanNo);	}
 function GGTRCC_FOWO___score()		{ return (this.mScore);		}
+
+//------------------------------------[GGTRCC_FOWO_MakeTable]-
+// Render the object in HTML
+//
+//	@param		aFOWs	IN	All the Fall-of-wickets
+//
+// 	@return		The HTML
+//
+//------------------------------------------------------------
+function GGTRCC_FOWO_MakeTable (aFOWs)
+{
+	var lRet="";
+	
+	if (0 != aFOWs.length)
+	{
+		//
+		// Table & header
+		//
+		lRet += "<table width='100%' border='0' cellspacing='0' cellpadding='0'>\n";
+		lRet += "  <tr class=\"GadgetBatsHeader\">\n";
+		lRet += "    <th>&nbsp;</th>";
+		lRet += "<th align='left'>Fall Of Wickets</th>";
+		lRet += "<th>&nbsp;</th>";
+		
+		//
+		// Mark as many wickets as we have ...
+		//
+		for (var i=0 ; i<aFOWs.length() ; i++)
+		{
+			lRet += "<th align='center'>" + (i+1) + " for</th>";
+		}
+
+		//
+		// And fill the rest with blanks
+		//
+		for (var i=aFOWs.length() ; i<10 ; i++)
+		{
+			lRet += "<th align='center'>&nbsp</th>";
+		}
+
+		lRet += "</tr>\n";
+		
+		//
+		// Now the line saying which batsmen were out
+		//
+		lRet += "<tr class=\"GadgetFixtureAltLine\">";
+		lRet += "<td>&nbsp;</td>";
+		lRet += "<td align='right'>Batsman Out</td>";
+		lRet += "<td>&nbsp;</td>";
+
+		for (var i=0 ; i<aFOWs.length() ; i++)
+		{
+			lRet += "<td align='center'>" + aFOWs[i].batsmanNo() + "</td>";
+		}
+
+		for (var i=aFOWs.length() ; i<10 ; i++)
+		{
+			lRet += "<td>&nbsp</th>";
+		}
+
+		lRet += "</tr>\n";
+		
+		//
+		// Now the line saying what the score was
+		//
+		lRet += "<tr class=\"FixtureAltLine\">";
+		lRet += "<td>&nbsp;</td>";
+		lRet += "<td align='right'>Score</td>";
+		lRet += "<td>&nbsp;</td>";
+
+		for (var i=0 ; i<aFOWs.length() ; i++)
+		{
+			lRet += "<td align='center'>" + aFOWs[i].score() + "</td>";
+		}
+
+		for (var i=aFOWs.length() ; i<10 ; i++)
+		{
+			lRet += "<td>&nbsp</th>";
+		}
+
+		lRet += "</tr>";
+
+		lRet += "</table>";
+	}
+	
+	return (lRet);
+}
