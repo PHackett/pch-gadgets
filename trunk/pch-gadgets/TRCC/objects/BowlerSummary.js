3562af60e073b4f3033d5f3cbd539b4485fdb521
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2008-04-07 19:28:04 +0000

    Lets see if we can render the bowlers too!

--- trunk/pch-gadgets/TRCC/objects/BowlerSummary.js
+++ trunk/pch-gadgets/TRCC/objects/BowlerSummary.js
@@ -32,11 +32,63 @@ function GGTRCC_BowlerSummaryO (aBowlerXML)
 }
 
 
-function GGTRCC_BowlerSummaryO___HTML()
+//-----------------------------[GGTRCC_BowlerSummaryO___HTML]-
+// Render the object in HTML
+//
+//	@param		aNum	IN		Bowler number
+//
+// 	@return		The HTML
+//
+//------------------------------------------------------------
+function GGTRCC_BowlerSummaryO___HTML (aNum)
 {
-	return ("Bowler " + this.mName + 
-			" Overs=" + this.mOvers + 
-			" Mdns=" + this.mMaidens + 
-			" Runs=" + this.mRuns + 
-			" Wkts=" + this.mWickets + "<br>");
+	var lRet="";
+
+	lRet +="<td align='left'>"		+ this.mName 	+ "</td>"	+
+			"<td align='right'>"	+ this.mOvers 	+ "</td>"	+ 
+			"<td align='right'>"	+ this.mMaidens + "</td>"	+ 
+			"<td align='right'>"	+ this.mRuns 	+ "</td>"	+ 
+			"<td align='right'>"	+ this.mWickets + "</td>"	+
+			"<td>&nbsp;</td>";
+
+	return (lRet);
+}
+
+
+//----------------------[GGTRCC_BowlerSummary_MakeMatchTable]-
+// Render the object in HTML
+//
+//	@param		aBowlerSummary	IN	All the bowlers
+//
+// 	@return		The HTML
+//
+//------------------------------------------------------------
+function GGTRCC_BowlerSummary_MakeMatchTable (aBowlerSummary)
+{
+	var lRet="";
+	
+	lRet += "<table width='100%' border='0' cellspacing='0' cellpadding='0'>\n";
+
+	lRet += "	<tr class=\"GadgetBatsHeader\">\n";
+	lRet += "		<th align='center'>Bowler</th>"		+ 
+					"<th align='right'>Overs</th>"		+
+					"<th align='right'>Maidens</th>"	+
+					"<th align='right'>Runs</th>"		+
+					"<th align='right'>Wickets</th>"	+
+					"<th width='" + gRTrail + "'>&nbsp;</th>";
+	lRet += "	</tr>\n";
+
+	//
+	// Down all the bowlers
+	//
+	for (var i=0 ; i<aBowlerSummary.length ; ++i)
+	{
+		lRet += (i % 2) ? "<tr>" : "<tr class=\"GadgetFixtureAltLine\">";
+		lRet += aBowlerSummary[i].HTML(i+1) + "\n";
+		lRet += "</tr>\n";
+	}
+
+	lRet += "</table>\n";
+
+	return (lRet);
 }
