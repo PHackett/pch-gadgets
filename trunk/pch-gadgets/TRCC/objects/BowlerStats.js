7aeacdc8a6f3c303494fd0810f5f9356ffaee22d
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2010-03-23 21:23:50 +0000

    Towards player stats

--- trunk/pch-gadgets/TRCC/objects/BowlerStats.js
+++ trunk/pch-gadgets/TRCC/objects/BowlerStats.js
@@ -1,4 +1,4 @@
-//------------------------------------[GGTRCC_BowlerSummaryO]-
+//------------------------------------------------------------
 // Object to hold year stats for a single bowler
 //
 // @param	aName	IN	Name of the bowler
@@ -9,5 +9,21 @@ function GGTRCC_BowlerStatsO (aName)
 	this.mBowlerSummary = new GGTRCC_BowlerSummaryO (null);
 	this.mGames			= 0;
 	this.mName			= aName;
+	
+	this.increment		= GGTRCC_BowlerStatsO___increment;
 }
 
+
+//------------------------------------------------------------
+// Increment the bowling stats given the information from a
+// single game
+//
+// @param	aBowlInningsData	IN	Data for one innings
+//
+//------------------------------------------------------------
+function GGTRCC_BowlerStatsO___increment (aBowlInningsData)
+{
+	//
+	// @todo
+	//
+}
