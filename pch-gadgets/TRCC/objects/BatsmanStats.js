d529f94142ad7542e09b6e0febabdb53df588502
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2010-03-23 21:23:50 +0000

    Towards player stats

--- pch-gadgets/TRCC/objects/BatsmanStats.js
+++ pch-gadgets/TRCC/objects/BatsmanStats.js
@@ -9,4 +9,21 @@ function GGTRCC_BatsmanStatsO (aName)
 	this.mBatterSummary = new GGTRCC_BatsmanInningsO (null);
 	this.mGames			= 0;
 	this.mName			= aName;
+
+	this.increment		= GGTRCC_BatsmanStatsO___increment;
+}
+
+
+//------------------------------------------------------------
+// Increment the batting stats given the information from a
+// single game
+//
+// @param	aBatInningsData	IN	Data for one innings
+//
+//------------------------------------------------------------
+function GGTRCC_BatsmanStatsO___increment (aBatInningsData)
+{
+	//
+	// @todo
+	//
 }
