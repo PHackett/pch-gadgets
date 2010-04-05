f32df442a1f9bb7cd39f18e0a31a6eea5b1dba90
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2010-04-05 19:17:42 +0000

    Begin the bowler year stats

--- trunk/pch-gadgets/TRCC/objects/BowlerStats.js
+++ trunk/pch-gadgets/TRCC/objects/BowlerStats.js
@@ -1,16 +1,28 @@
 //------------------------------------------------------------
-// Object to hold year stats for a single bowler
+// Object to hold year stats for a single bowler.
+// This is the equivalent of the GGTRCC_BatsmanSummaryO. It's
+// called GGTRCC_BowlerStatsO because GGTRCC_BowlerSummaryO
+// is the object into which the actual game data is read.
+// A bit confusing, I know ..
 //
 // @param	aName	IN	Name of the bowler
 //
 //------------------------------------------------------------
 function GGTRCC_BowlerStatsO (aName)
 {
-	this.mBowlerSummary = new GGTRCC_BowlerSummaryO (null);
-	this.mGames			= 0;
 	this.mName			= aName;
 	
-	this.increment		= GGTRCC_BowlerStatsO___increment;
+	this.mGames			= 0;
+	this.mOvers			= 0;
+	this.mMaidens		= 0;
+	this.mRuns			= 0;
+	this.mWickets		= 0;
+	this.mFivePlus		= 0;
+	
+	this.update			= GGTRCC_BowlerStatsO___update;
+	this.getAverage		= GGTRCC_BowlerStatsO___getAverage;
+	this.getHTMLAverage = GGTRCC_BowlerStatsO___getHTMLAverage;
+	this.HTML			= GGTRCC_BowlerStatsO___HTML;
 }
 
 
@@ -18,12 +30,63 @@ function GGTRCC_BowlerStatsO (aName)
 // Increment the bowling stats given the information from a
 // single game
 //
-// @param	aBowlInningsData	IN	Data for one innings
+// @param	aBowlInningsData	IN	Data for one innings (GGTRCC_BowlerSummaryO)
 //
 //------------------------------------------------------------
-function GGTRCC_BowlerStatsO___increment (aBowlInningsData)
+function GGTRCC_BowlerStatsO___update (aBowlInningsData)
 {
-	//
-	// @todo
-	//
+	this.mGames++;
+	
+	this.mOvers 	= TRCCUtils_OversAdd (this.mOvers, aBowlInningsData.Overs);
+	this.mMaidens	+= aBowlInningsData.mMaidens;
+	this.mRuns		+= aBowlInningsData.mRuns;
+	this.mWickets	+= aBowlInningsData.mWickets;
+	
+	if (aBowlInningsData.mWickets >= 5)
+	{
+		this.mFivePlus++;
+	}
+}
+
+
+//------------------------------------------------------------
+//
+//------------------------------------------------------------
+function GGTRCC_BowlerStatsO___getAverage()
+{
+	var lRet=0.0;
+
+	if (this.mWickets > 0)
+	{
+		lRet = this.mRuns / this.mWickets;
+	}
+
+	return (lRet);	
+}
+
+
+function GGTRCC_BowlerStatsO___getHTMLAverage()
+{
+	var lRet="";
+
+	if (this.mWickets > 0)
+	{
+		// 2 decimal places for HTML
+		lRet = GGUtils_numToString (this.getAverage(), 2);
+	}
+	else
+	{
+		lRet = "&nbsp;";
+	}
+
+	return (lRet);		
+}
+
+
+function GGTRCC_BowlerStatsO___HTML()
+{
+	var lRet="To be implemented";
+	
+	
+	Return (lRet);
 }
