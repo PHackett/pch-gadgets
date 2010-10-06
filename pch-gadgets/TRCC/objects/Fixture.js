e6013df12d1c0a6222e6fa0077f0ef77d7228524
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2010-10-06 20:06:00 +0000

    Alter the URL we generate for links to matches from the "Fixtures" page to <yyyy>_<month>_<dd>_<oppo> - All lowercase

--- pch-gadgets/TRCC/objects/Fixture.js
+++ pch-gadgets/TRCC/objects/Fixture.js
@@ -361,8 +361,9 @@ function GGTRCC_FixtureO___URL()
 		lURL += "All-Fixtures/";
 		lURL += this.mDate.getFullYear() + "/";
 		lURL += this.getTeam() + "/";
-		lURL += GGUtils_MonthStringFromDate (this.mDate).toLowerCase() + "_" +
-				GGUtils_GetNumAsOrdinalString (this.mDate.getDate(), false) + "_" +
+		lURL += this.mDate.getFullYear() + "_" +
+		        GGUtils_MonthStringFromDate (this.mDate).toLowerCase() + "_" +
+				this.mDate.getDate() + "_" +
 				this.mOppo.toLowerCase();
 	}
 	else 
