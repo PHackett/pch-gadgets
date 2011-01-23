62d7abdc1bca15af245162dd6acbf9e19554bcf8
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-01-23 19:07:11 +0000

    Rendering of player lifetime stats

---
+++ trunk/pch-gadgets/TRCC/objects/PlayerLifetime.js
@@ -0,0 +1,11 @@
+//
+// This file contains methods for parsing & rendering of the lifetime stats for individual players
+//
+function GGTRCC_PlayerLifetime_GetXMLURLFromLocation()
+{
+	var lRet = "";
+	
+	lRet = "this is a dummy URL";
+	
+	return (lRet);
+}
