9f591bb10f5aafd86bce90f42b3cc6c359e469bb
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-01-23 19:43:42 +0000

    Continue development

--- trunk/pch-gadgets/TRCC/objects/PlayerLifetime.js
+++ trunk/pch-gadgets/TRCC/objects/PlayerLifetime.js
@@ -6,8 +6,7 @@ function GGTRCC_PlayerLifetime_GetXMLURLFromLocation()
 	var lXMLURL=gGGGadget_Root + "TRCC/data/players/";
 	
 	lXMLURL += "(0) " + GGTRCC_ItemFromSitesURL (0) +
-				" (1) " + GGTRCC_ItemFromSitesURL (1) + 
-				" (2) " + GGTRCC_ItemFromSitesURL (2);
+				" (1) " + GGTRCC_ItemFromSitesURL (1);
 	
 	return (lXMLURL);
 }
