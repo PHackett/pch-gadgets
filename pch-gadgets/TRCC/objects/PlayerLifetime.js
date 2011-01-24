e8becfd93fccf93542eb306d7c7091b23f8b0b77
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-01-24 19:34:58 +0000

    Continue development

--- pch-gadgets/TRCC/objects/PlayerLifetime.js
+++ pch-gadgets/TRCC/objects/PlayerLifetime.js
@@ -26,3 +26,46 @@ function GGTRCC_PlayerLifetime_GetXMLURLFromLocation()
 	
 	return (lXMLURL);
 }
+
+
+//-----------------------------------[GGTRCC_PlayerLifetimeO]-
+// Object to hold player lifetime stats
+//
+// @param aCricketMatchXML	IN 	The <PlayerStats> XML node
+//
+//------------------------------------------------------------
+function GGTRCC_PlayerLifetimeO (aPsXML)
+{
+	//
+	// Members
+	//
+	this.mName		= null;
+	this.Generated	= null;
+	
+	//
+	// Get the root object
+	//
+	var lPLS=aPsXML.getElementsByTagName("PlayerStats").item(0);
+
+	//
+	// Extract the data from the attributes
+	//
+	this.mName		= lPLS.getAttribute ("name");
+	this.Generated	= new Date (lPLS.getAttribute ("generated"));
+	
+	//
+	// Methods
+	//
+	this.playerHTML 		= GGTRCC_PlayerLifetimeO___playerHTML;
+}
+
+
+function GGTRCC_PlayerLifetimeO___playerHTML()
+{
+	var lRet = "";
+	
+	lRet += "Name = " 		+ this.mName			+ "<br>";
+	lRet += "Generated = "	+ this.mDate.toString()	+ "<br>";
+	
+	return (lRet);
+}
