40e13a096dbd6654e1d0a6308d49cea6e2908df2
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-01-25 20:42:13 +0000

    Continue development

--- pch-gadgets/TRCC/objects/PlayerLifetime.js
+++ pch-gadgets/TRCC/objects/PlayerLifetime.js
@@ -162,6 +162,7 @@ function GGTRCC_PLBowlingO___HTML()
 		
 		lRet += "*2this.mBowlingHighlights.length=" + this.mBowlingHighlights.length + "*<br>";
 		lRet += gXXXPLBowlingDebug + "<br>";
+		gXXXPLBowlingDebug = "";
 		
 		for (var i=0 ; i<this.mBowlingHighlights.length ; ++i)
 		{
