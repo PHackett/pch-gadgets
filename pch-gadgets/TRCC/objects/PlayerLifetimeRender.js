888b8a9244f4ac9117a2b1dc5e12a9c4f40a465f
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-01-28 21:02:06 +0000

    Continue development

--- pch-gadgets/TRCC/objects/PlayerLifetimeRender.js
+++ pch-gadgets/TRCC/objects/PlayerLifetimeRender.js
@@ -14,6 +14,73 @@ function GGTRCC_RenderBowlingByYear (aPLSO)
 {
 	var lRet="";
 	
+	lRet += "<span class='GadgetStatsHeading'>Bowling By Year</span>";
+	
+	lRet += "<table width='100%' cellSpacing='0' cellPadding='0' border='0'>";
+	lRet +=   "<thead>";
+	lRet +=     "<tr class=\"GadgetBatsHeader\">";
+	lRet +=       "<th>&nbsp;</th>";
+	lRet +=       "<th align='left'>Year</th>";
+	lRet +=       "<th align='right'>Games</th>";
+	lRet +=       "<th align='right'>Overs</th>";
+	lRet +=       "<th align='right'>Maidens</th>";
+	lRet +=       "<th align='right'>Runs</th>";
+	lRet +=       "<th align='right'>Wickets</th>";
+	lRet +=       "<th align='right'>5+</th>";
+	lRet +=       "<th align='right'>Runs/Over</th>";
+	lRet +=       "<th align='right'>Strike Rate</th>";
+	lRet +=       "<th align='right'>Average</th>";
+	lRet +=       "<th>&nbsp;</th>";
+	lRet +=     "</tr>";
+	lRet +=   "</thead>";
+
+	//
+	// Down all the years
+	//
+	for (i=0 ; i<aPLSO.mYears.length ; i++)
+	{
+		var lYearData=aPLSO.mYears[i];
+		var lBYS=lYearData.mBowling;
+
+		if (lYearData.mBowlingYearStats != null)
+		{
+			lRet += "<tr>";
+			lRet +=   "<td>&nbsp;</td>";
+			lRet +=   "<td>" + lYearData.mYear + "</td>";
+			lRet +=   "<td>" + lBYS.mGames + "</td>";
+			
+			if (lBYS.mBowlingData != null)
+			{
+				lRet +=   "<td>" + lBYS.mBowlingData.mOvers		+ "</td>";
+				lRet +=   "<td>" + lBYS.mBowlingData.mMaidens	+ "</td>";
+				lRet +=   "<td>" + lBYS.mBowlingData.mRuns		+ "</td>";
+				lRet +=   "<td>" + lBYS.mBowlingData.mWickets	+ "</td>";
+				lRet +=   "<td>" + lBYS.mFivePlus				+ "</td>";		
+				
+				lRet +=   "<td>R/O</td>";		
+				lRet +=   "<td>S/R</td>";		
+				lRet +=   "<td>Avg</td>";		
+				
+			}
+			else
+			{
+				lRet +=   "<td>&nbsp;</td>";
+				lRet +=   "<td>&nbsp;</td>";
+				lRet +=   "<td>&nbsp;</td>";
+				lRet +=   "<td>&nbsp;</td>";
+				lRet +=   "<td>&nbsp;</td>";
+				lRet +=   "<td>&nbsp;</td>";
+				lRet +=   "<td>&nbsp;</td>";
+				lRet +=   "<td>&nbsp;</td>";
+			}
+			
+			
+				lYearData.mBowlingYearStats.HTML();
+			lRet += "</tr>";
+		}
+	}
+
+	lRet += "</table>";
 	
 	return (lRet);
 }
