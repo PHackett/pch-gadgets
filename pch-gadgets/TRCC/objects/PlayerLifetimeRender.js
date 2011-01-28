33f2f6905f77b89901e0b6a51f229fe24966932f
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-01-28 19:40:53 +0000

    Begin the rendering process.... this is going to take some time!

---
+++ pch-gadgets/TRCC/objects/PlayerLifetimeRender.js
@@ -0,0 +1,20 @@
+//
+// Functions for the renderingof Player Career stats data
+// See also PlayerLifetime.js
+//
+
+
+//-------------------------------[GGTRCC_RenderBowlingByYear]-
+// For the rendering of bowling by year for player stats 
+//
+// @param aPSO	IN 	The GGTRCC_PlayerLifetimeO object
+//
+//------------------------------------------------------------
+function GGTRCC_RenderBowlingByYear (aPLSO)
+{
+	var lRet="";
+	
+	
+	return (lRet);
+}
+ 
