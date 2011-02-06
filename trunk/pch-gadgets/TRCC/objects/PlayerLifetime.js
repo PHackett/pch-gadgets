9644f1cd6c4a98a68b252a30ab70237fa6fea671
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-02-06 16:56:16 +0000

    More summarising the lifetime stats

--- trunk/pch-gadgets/TRCC/objects/PlayerLifetime.js
+++ trunk/pch-gadgets/TRCC/objects/PlayerLifetime.js
@@ -598,7 +598,7 @@ function GGTRCC_PlayerLifetimeO (aPsXML)
 	
 	if (0 != lBatStats1969to1997.length)
 	{
-		this.mBattingStats1969to1997 = new GGTRCC_PLBattingO (lBatStats1969to1997[0]);
+		this.mBattingStats1969to1997 = new GGTRCC_PLBattingO (lBatStats1969to1997[0].getElementsByTagName("Batting"));
 	}
 	
 	//
@@ -608,7 +608,7 @@ function GGTRCC_PlayerLifetimeO (aPsXML)
 	
 	if (0 != lBowlStats1969to1997.length)
 	{
-		this.mBowlingStats1969to1997 = new GGTRCC_PLBowlingO (lBowlStats1969to1997[0]);
+		this.mBowlingStats1969to1997 = new GGTRCC_PLBowlingO (lBowlStats1969to1997[0].getElementsByTagName("Bowling"));
 	}
 	
 	//
