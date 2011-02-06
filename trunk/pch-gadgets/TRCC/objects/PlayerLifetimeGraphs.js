632230a0ea580c75c6d4476c2745361998197249
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-02-06 19:52:46 +0000

    More summarising the lifetime stats - This time the Graphs

--- trunk/pch-gadgets/TRCC/objects/PlayerLifetimeGraphs.js
+++ trunk/pch-gadgets/TRCC/objects/PlayerLifetimeGraphs.js
@@ -20,19 +20,19 @@ function Sort___ByDate (aOne, aTwo)
 //	@param	aRuns		IN	Runs in year
 //	@param	aAverage	IN	Batting average for this year
 //
-function GGTRCC_PlayerLTGraph_BattingItemO (aYear, aRuns, aAverage)
+function GGTRCC_PlayerLTGraph_ItemO (aYear, aOne, aTwo)
 {
 	//
 	// Members
 	//
-	this.mYear		= aYear;
-	this.mRuns		= aRuns;
-	this.mAverage	= aAverage;
+	this.mYear	= aYear;
+	this.mOne	= aOne;
+	this.mTwo	= aTwo;
 }
 
 
 //
-// Make the array of GGTRCC_PlayerLTGraph_BattingItemO objects 
+// Make the array of GGTRCC_PlayerLTGraph_ItemO objects 
 // from the GGTRCC_PlayerLifetimeO
 //
 function GGTRCC_PlayerLTGraph_MakeBattingArray (aPLSO)
@@ -49,9 +49,9 @@ function GGTRCC_PlayerLTGraph_MakeBattingArray (aPLSO)
 		{
 			var lCompInns = (aPLSO.mYears[i].mBatting.mInnings - 0) - (aPLSO.mYears[i].mBatting.mNotOuts - 0);
 			
-			lTmpA[lTmpA.length] = new GGTRCC_PlayerLTGraph_BattingItemO ((aPLSO.mYears[i].mYear - 0),
-																		 (aPLSO.mYears[i].mBatting.mRuns - 0),
-																		 TRCCUtils_getAverage (lCompInns, (aPLSO.mYears[i].mBatting.mRuns - 0)));
+			lTmpA[lTmpA.length] = new GGTRCC_PlayerLTGraph_ItemO ((aPLSO.mYears[i].mYear - 0),
+																  (aPLSO.mYears[i].mBatting.mRuns - 0),
+																  TRCCUtils_getAverage (lCompInns, (aPLSO.mYears[i].mBatting.mRuns - 0)));
 		}
 	}
 	
@@ -75,10 +75,48 @@ function GGTRCC_PlayerLTGraph_MakeBattingArray (aPLSO)
 		{
 			for (var j=(lPrevItem.mYear + 1) ; j<lTmpA[i].mYear ; ++j)
 			{
-				lRet[lRet.length] = new GGTRCC_PlayerLTGraph_BattingItemO (j, 0, -1);
+				lRet[lRet.length] = new GGTRCC_PlayerLTGraph_ItemO (j, 0, -1);
 			}
 			
 			lPrevItem = lRet[lRet.length] = lTmpA[i];
+		}
+		
+		//
+		// Turn the "runs" into "cumulative runs"
+		//
+		var lCum=0;
+		
+		for (var i=0 ; i<lRet.length ; ++i)
+		{
+			lRet[i].mOne = lRet[i].mOne + lCum;
+			lCum = lRet[i].mOne;
+		}
+		
+		//
+		// Adjust the totals to take account of the statistice 
+		// that david Downes compiled for the years 1969 to 1997
+		// as these span a far wider time
+		//
+		if (null != aPLSO.mBattingStats1969to1997)
+		{
+			var lAdjust=-1;
+			
+			for (var i=0 ; i<lRet.length ; ++i)
+			{
+				if (lRet[i].mYear == 97)
+				{
+					lAdjust = (aPLSO.mBattingStats1969to1997.mRuns - 0) - lRet[i].mOne;
+					break;
+				}
+			}
+			
+			if (lAdjust > 0)
+			{
+				for (var i=0 ; i<lRet.length ; ++i)
+				{
+					lRet[i].mOne += lAdjust;
+				}
+			}
 		}
 	}
 	
