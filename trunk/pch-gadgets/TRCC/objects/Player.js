7e7a09d57eedd4a15926aebef208049f161e5835
paulhackettuk@googlemail.com <paulhackettuk@googlemail.com@d9397a04-f348-0410-9d37-6dfab9dad63f>
2011-02-26 12:39:59 +0000

    Debug: What is happening on IE?

--- trunk/pch-gadgets/TRCC/objects/Player.js
+++ trunk/pch-gadgets/TRCC/objects/Player.js
@@ -122,7 +122,7 @@ function GGTRCC_Player___HTML()
 	
 	lURL += GGGadget_getHostingRoot() + "players/" + this.Canonical();
 	
-	return (GGUtils_makeHREF (this.Name(), lURL));
+	return (GGUtils_makeHREF (this.Name() + "[" + this.Canonical() + "]", lURL));
 }
 
 
