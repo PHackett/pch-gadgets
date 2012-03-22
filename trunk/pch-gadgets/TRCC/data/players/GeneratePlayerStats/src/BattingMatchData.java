c7fbdd65cb973b82dd4e7db77438e935de30134d
paulhackettuk <paulhackettuk@d9397a04-f348-0410-9d37-6dfab9dad63f>
2012-03-22 20:14:38 +0000

    and further again ....

--- trunk/pch-gadgets/TRCC/data/players/GeneratePlayerStats/src/BattingMatchData.java
+++ trunk/pch-gadgets/TRCC/data/players/GeneratePlayerStats/src/BattingMatchData.java
@@ -21,63 +21,58 @@ public class BattingMatchData
 	public MatchID				MatchID()	{ return (mMatchID);		}
 	public BatterGameSummary	GetBGS()	{ return (mGameData);		}
 	
-	public boolean ParseFromXML (Element aBattingElement, String aKey)
+	/**
+	 * 
+	 * @param aBattingElement
+	 * @param aKey
+	 * @return
+	 */
+	public boolean ParseFromXML (Element aBattingElement)
 	{
 		boolean 	lRet=false;
-        NodeList	lBMD=aBattingElement.getElementsByTagName(aKey);
-        
-        if (lBMD.getLength() != 1)
+		
+    	//
+    	// First matchID - "<MatchId date="Sun Aug 1 00:00:00 UTC+0100 2010" oppo="Theale and Tilehurst"/>"
+    	//
+    	NodeList	lMID=aBattingElement.getElementsByTagName("MatchId");
+    	
+        if (lMID.getLength() != 1)
         {
-        	System.out.println ("ERROR: We have " + lBMD.getLength() + " '" + aKey + "' records");
+        	System.out.println ("ERROR: We have " + lMID.getLength() + " 'MatchId' records");
         	lRet = false;	        	
         }
         else
         {
-        	Element lBElement = (Element)lBMD.item(0);
-
-        	//
-        	// First matchID - "<MatchId date="Sun Aug 1 00:00:00 UTC+0100 2010" oppo="Theale and Tilehurst"/>"
-        	//
-        	NodeList	lMID=lBElement.getElementsByTagName("MatchId");
+        	Element lMIDElement = (Element)lMID.item(0);
+        	mMatchID 			= new MatchID();
         	
-            if (lMID.getLength() != 1)
-            {
-            	System.out.println ("ERROR: We have " + lMID.getLength() + " 'MatchId' records");
+        	if (!mMatchID.parseFromYearStatsXML (lMIDElement))
+        	{
+        		System.out.println ("ERROR: Failed to parse MatchID");
             	lRet = false;	        	
-            }
-            else
-            {
-            	Element lMIDElement = (Element)lMID.item(0);
-            	mMatchID 			= new MatchID();
-            	
-            	if (!mMatchID.parseFromYearStatsXML (lMIDElement))
-            	{
-            		System.out.println ("ERROR: Failed to parse MatchID");
-                	lRet = false;	        	
-            	}
-            }
-            
-            //
-            // Batting data - "<BattingData runs="7" notouts="0"/>"
-            //
-        	NodeList	lBD=lBElement.getElementsByTagName("BattingData");
+        	}
+        }
+        
+        //
+        // Batting data - "<BattingData runs="7" notouts="0"/>"
+        //
+    	NodeList	lBD=aBattingElement.getElementsByTagName("BattingData");
+    	
+        if (lBD.getLength() != 1)
+        {
+        	System.out.println ("ERROR: We have " + lMID.getLength() + " 'BattingData' records");
+        	lRet = false;	        	
+        }
+        else
+        {
+        	Element lMIDElement = (Element)lBD.item(0);
+        	mGameData			= new BatterGameSummary();
         	
-            if (lBD.getLength() != 1)
-            {
-            	System.out.println ("ERROR: We have " + lMID.getLength() + " 'BattingData' records");
+        	if (!mGameData.LoadFromYearStatsXML(lMIDElement))
+        	{
+        		System.out.println ("ERROR: Failed to parse BattingData");
             	lRet = false;	        	
-            }
-            else
-            {
-            	Element lMIDElement = (Element)lBD.item(0);
-            	mGameData			= new BatterGameSummary();
-            	
-            	if (!mGameData.LoadFromYearStatsXML(lMIDElement))
-            	{
-            		System.out.println ("ERROR: Failed to parse BattingData");
-                	lRet = false;	        	
-            	}
-            }
+        	}
         }
 		
 		return (lRet);
